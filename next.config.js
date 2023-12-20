df6cc1be2ec639e468ad0a26c6cab68338bd7b6f
Derin Tolu <derintolu@me.com>
2023-12-20 02:48:24 -0800

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withBuilderDevTools = require("@builder.io/dev-tools/next")();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBuilderDevTools({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
